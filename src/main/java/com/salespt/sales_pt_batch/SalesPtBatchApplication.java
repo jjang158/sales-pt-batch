526b9a9384939f7b3899379c79c17de4ba2015b0
eyjang <sa358@naver.com>
2025-09-14 16:46:53 +0900

    create project

---
+++ src/main/java/com/salespt/sales_pt_batch/SalesPtBatchApplication.java
@@ -0,0 +1,13 @@
+package com.salespt.sales_pt_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesPtBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesPtBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salespt/sales_pt_batch/SalesPtBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salespt.sales_pt_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPtBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
